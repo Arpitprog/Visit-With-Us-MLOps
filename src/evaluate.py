a114b89a532ea8dd13c5f0b21998cafc071e760c
Arpitprog <57315611+Arpitprog@users.noreply.github.com>
2026-08-02 23:48:30 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import joblib
 
+from sklearn.metrics import accuracy_score
+
+test = pd.read_csv("test.csv")
+
+X = test.drop("ProdTaken", axis=1)
+y = test["ProdTaken"]
+
+model = joblib.load("model.pkl")
+
+pred = model.predict(X)
+
+accuracy = accuracy_score(y, pred)
+
+print("Accuracy:", accuracy)

fda266a103428921f8452d4cb503aeff41bf44c2
Arpitprog <57315611+Arpitprog@users.noreply.github.com>
2026-08-02 23:47:57 +0530

    Add data preparation script for train-test split

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
+df = pd.read_csv("data/tourism.csv")
+
+df = df.drop(columns=["Unnamed: 0", "CustomerID"])
+
+train, test = train_test_split(
+    df,
+    test_size=0.20,
+    random_state=42,
+    stratify=df["ProdTaken"]
+)
+
+train.to_csv("train.csv", index=False)
+test.to_csv("test.csv", index=False)
+
+print("Train and Test datasets created successfully.")
